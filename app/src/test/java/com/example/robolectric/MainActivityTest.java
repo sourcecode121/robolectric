fd988bd448938f059f88c4aa3d634ec6c3609f37
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-10-05 12:33:56 +0530

    Updating the test package

--- app/src/test/java/com/example/robolectric/MainActivityTest.java
+++ app/src/test/java/com/example/robolectric/MainActivityTest.java
@@ -1,11 +1,14 @@
 package com.example.robolectric;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static com.example.robolectric.support.ResourceLocator.getString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 /**
@@ -14,9 +17,21 @@ import static org.junit.Assert.*;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 22)
 public class MainActivityTest {
+
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
     @Test
-    public void shouldNotBeNull() {
-        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+    public void shouldNotBeNull() throws Exception {
         assertNotNull(activity);
+    }
+
+    @Test
+    public void shouldHaveTitle() throws Exception {
+        assertThat(activity.getTitle().toString(), equalTo(getString(R.string.app_name)));
     }
 }
---
+++ app/src/test/java/com/example/robolectric/support/ResourceLocator.java
@@ -0,0 +1,13 @@
+package com.example.robolectric.support;
+
+import org.robolectric.RuntimeEnvironment;
+
+/**
+ * Created by Anand on 05/10/2016.
+ */
+
+public class ResourceLocator {
+    public static String getString(int stringId) {
+        return RuntimeEnvironment.application.getString(stringId);
+    }
+}
